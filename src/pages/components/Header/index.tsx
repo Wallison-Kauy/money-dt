280ebf9317e13b57fcf85f738d2ebb662eaec5dc
Wallison Kauy <wallisonkauy@gmail.com>
2023-04-21 18:10:52 -0300

    Wip: componente Header

---
+++ src/pages/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image"
+import logo from "../../../../public/imagens/header.svg"
+import * as S from "./styles"
+
+export function Header(){
+  return(
+
+    <S.Container className="w-full bg-gray-900 h-52 pt-11 px-3">
+   
+      <header className="container flex mx-auto w-full h-12 justify-between">
+        <Image 
+          src={logo.src} 
+          alt="Logo DT Money"
+          width={172}
+          height={41}
+        />
+        <button className="
+          bg-green-500 
+            w-40 
+            h-12 
+            px-5 
+            py-3 
+            rounded-md 
+            text-xs 
+            font-bold
+            text-write
+          ">
+           Nova transação
+        </button>
+      </header>
+
+    </S.Container>
+  
+
+  )
+}
+
+export default Header
