905158c87ebab195c20c58fe07cb250966d540f9
Wallison Kauy <wallisonkauy@gmail.com>
2023-06-12 22:27:32 -0300

    recofing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -13,29 +13,6 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-    colors: {
-      'write': "#FFFFFF",
-      'gray': {
-        "100" : "#E1E1E6",
-        "300" : "#C4C4CC",
-        "400" : "#8D8D99",
-        "500" : "#7C7C8A",
-        "600" : "#323238",
-        "700" : "#29292E",
-        "800" : "#202024",
-        "900" : "#121214",
-      },
-      "green" : {
-        "300": "#00B37E",
-        "500": "#00875F",
-        "700": "#015F43",
-      },
-      'red': {
-        "300" : "#F75A68",
-        "500" : "#AB222E",
-        "700" : "#7A1921",
-      },
-    }
   },
   plugins: [],
 }
